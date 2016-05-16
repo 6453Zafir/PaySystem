f64a5375aaefc40495bc22f210172cc757f1e888
6453Zafir <511599737@qq.com>
2016-05-16 19:15:47 +0800

    main activity

--- Payment/app/src/main/java/com/example/admin/payment/MainActivity.java
+++ Payment/app/src/main/java/com/example/admin/payment/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.admin.payment;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button inputPhoneNumBtn;
+    public EditText inputPhomeNumtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputPhoneNumBtn = (Button)findViewById(R.id.submitPhoneNum);
+        inputPhomeNumtext =(EditText)findViewById(R.id.phoneNum);
+
+        inputPhoneNumBtn.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        isphoneNumExist(inputPhomeNumtext.getText().toString());
+                    }
+                }
+        );
+    }
+
+    public boolean isphoneNumExist(String phoneNum){
+    return true;
     }
 }
