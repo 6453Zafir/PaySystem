e03f61885a646b50b0a7cc013c41a7eae805dac5
maerye <350046243@qq.com>
2016-05-16 19:19:05 +0800

    eee

--- Payment/app/src/main/java/com/example/admin/payment/MainActivity.java
+++ Payment/app/src/main/java/com/example/admin/payment/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.admin.payment;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.SystemClock;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.File;
 
+public class MainActivity extends AppCompatActivity {
+    private SQLiteDatabase db;
+   private PaymentDatabaseHelper databaseHelper;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper=new PaymentDatabaseHelper(getApplicationContext(),"payment",1);
+      //  queryRecord();
+    }
+
+    private void queryRecord(String phonenumber)
+    {
+        Cursor cursor=databaseHelper.getReadableDatabase().rawQuery("select * from payrecord",null);
+        while (cursor.moveToNext())
+        {
+            System.out.println(cursor.getInt(1));
+            System.out.println(cursor.getString(2));
+        }
     }
 }
---
+++ Payment/app/src/main/java/com/example/admin/payment/PaymentDatabaseHelper.java
@@ -0,0 +1,42 @@
+package com.example.admin.payment;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by 马二爷 on 2016/5/16.
+ */
+public class PaymentDatabaseHelper extends SQLiteOpenHelper {
+
+    final String SQL_CREATE_TABLE="create table payrecord(id integer primary key autoincrement," +
+                                                                 "phonenumber varchar(20)," +
+                                                                 "year integer" +
+                                                                 ",month integer," +
+                                                                 "minute BIGINT," +
+                                                                 "hasPaid BOOLEAN)";
+    public PaymentDatabaseHelper(Context context,String name,int version)
+    {
+        super(context,name,null,version);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase database)
+    {
+        Object [] initial={111,2004,1,150,true};
+        Object []ini2={111,2004,2,300,true};
+        database.execSQL(SQL_CREATE_TABLE);
+        database.execSQL("insert into payrecord values(null,?,?,?,?,?)",initial);
+        database.execSQL("insert into payrecord values(null,?,?,?,?,?)",ini2);
+
+    }
+    @Override
+    public void onUpgrade(SQLiteDatabase database,int oldversion, int newversion)
+    {
+        System.out.println("call update");
+    }
+    @Override
+    public void onOpen(SQLiteDatabase database)
+    {
+        super.onOpen(database);
+    }
+}
